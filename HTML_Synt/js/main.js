574fcfba5e17434ca406e9cc4612adae9c6e45ba
cdfelton <103611093+cdfelton@users.noreply.github.com>
2023-03-28 14:08:22 -0700

    Updated Experiment, Began Work on R Pipeline

--- HTML_Synt/js/main.js
+++ HTML_Synt/js/main.js
@@ -1,11 +1,35 @@
 
 let practiceTests = [
-  { A: "Mike is asleep or not awake.", B:"Based on the sentence, how possible is it that Mike is both asleep and not awake?" , Explanation: "To us, after reading the sentence it seems very possible that Mike is both asleep and not awake. We recommend somewhere on the right end of the slider, maybe near the end."},
-  { A: "David is a bachelor or is married.", B:'Based on the sentence, how possible is it that David is both a bachelor and married?', Explanation: "This seems much less possible to us, so we recommend an answer at the far left end of the slider." },
+  { A: "Mike is a professor but not a linguist.", B:"Based on the sentence, how possible is it that Mike is both a professor and a linguist?" , Explanation: "To us, after reading the sentence it seems impossible that Mike is both a professor and a linguist. We recommend somewhere on the left end of the slider, maybe near the end."},
+  { A: "David is a chef and a business owner.", B:'Based on the sentence, how possible is it that David is both a chef and a business owner?', Explanation: "To us, it seems like this must be the case, so we recommend an answer at the far right end of the slider." },
   { A: "Feel free to take a free T-shirt or hat.", B: "Based on the sentence, how possible is it that the speaker meant you could take both a T-shirt and a hat?", Explanation: "We're not sure about this one, so just answer based on your intuitions."}
 ]
 
-let GIST_LINK = 'https://gist.githubusercontent.com/cdfelton/2d813e89cddc55f735872dad6ba62e84/raw/9f74c6a463c75ddd49b25da1919f304664642891/stimuli_2.json';
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
+
+let GIST_LINK;
+
+let group = getRandomIntInclusive(1, 4)
+switch (group) {
+  case 1: GIST_LINK = 'https://gist.githubusercontent.com/cdfelton/bc05b43aa99a79b7e3c41d827fef91ac/raw/4dbde196c7b411a21a1740fc5c5d720fe11c8775/SynDis_LS1.json';
+    break;
+
+  case 2: GIST_LINK ='https://gist.githubusercontent.com/cdfelton/282545dd162718d6833374f5c2793969/raw/e91804c0694db8691ed0493084a2aa74ae49e861/SynDis_LS2.json';
+    break;
+
+  case 3: GIST_LINK ='https://gist.githubusercontent.com/cdfelton/eceb7e68dcbf539ef5f327de09bb7af1/raw/edd1284fa5c07175954f7b4cdef52ad5a2432ee1/SynDis_LS3.json';
+    break;
+
+  case 4: GIST_LINK ='https://gist.githubusercontent.com/cdfelton/b92612f1bd22bb01c8448ed331c5ee8e/raw/a90e0779c7f6346d5ba716fb078363a555c0020e/SynDis_LS4.json';
+    break;
+}
+
+
+
 let stimuli;
 userResult = [];
 let sliderMoved = false;
@@ -14,8 +38,8 @@ let originalLength;
 
 init();
 
-function Result(id, chance) {
-  this.id = id;
+function Result(Condition_Item, chance) {
+  this.Condition_Item = Condition_Item;
   this.chance = chance;
 }
 
@@ -134,13 +158,13 @@ function populateTest() {
   // document.getElementById('valOne').innerHTML = 50;
   sliderMoved = false;
   item = stimuli.shift();
-  document.getElementById('realRowOne').innerHTML = item.Disjunction;
+  document.getElementById('realRowOne').innerHTML = item.Sentence;
   document.getElementById('realRowTwo').innerHTML = item.Question;
 }
 
 function submitReal() {
   if (stimuli.length == 0) { // check to see if we are out of stimuli
-    userResult.push(new Result(item.ID,
+    userResult.push(new Result(item.Condition_Item,
       document.getElementById('realOne').value
     ));
     console.log(userResult)
@@ -156,7 +180,7 @@ function submitReal() {
     else {
       document.getElementById('progressbar').style.width = 100*(originalLength - stimuli.length)/originalLength + "%";
       console.log(item);
-      userResult.push(new Result(item.ID,
+      userResult.push(new Result(item.Condition_Item,
         document.getElementById('realOne').value
       ));
       console.log(userResult);
